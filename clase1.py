076371be5b024302d78cf9a58eabec14c7dca947
jeinzanderson-lang <jeinzanderson@gmail.com>
2026-02-07 16:36:16 +0000

    primer trabajo en clase

---
+++ clase1.py
@@ -0,0 +1,17 @@
+edad = int(input("ingrese la edad: "))
+
+if edad >= 18:
+    print("Es permitido")
+else:
+    print("no es permitido")
+print("la evaluación de la expresión es: ", edad>=18)
+
+
+if edad <= 18:
+    print("Es permitido")
+else:
+    print("no es permitido")
+print("la evaluación de la expresión es: ", edad<=18)
+
+
+
